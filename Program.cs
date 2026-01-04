ee0784cef9eb451d614cb26960ae0e8754ce98c9
Jacob Craver <JacobYCraver@outlook.com>
2026-01-04 18:25:57 -0500

    Added link-back to github repo

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using CodeTextToolkit;
-using CodeTextToolkit.Services;
+using CodeFlowLLMToolkit;
+using CodeFlowLLMToolkit.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
--- Services/FileProcessingService.cs
+++ Services/FileProcessingService.cs
@@ -1,7 +1,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace CodeTextToolkit.Services;
+namespace CodeFlowLLMToolkit.Services;
 
 public class FileProcessingService
 {
